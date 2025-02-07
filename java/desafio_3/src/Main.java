5e275001e5a8fbbedb12bbdb418980a21e39632f
Gabriel Carmona <gabrielrache123@hotmail.com>
2025-02-07 00:29:01 -0300

    Conclusão do desafio 3

---
+++ java/desafio_3/src/Main.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        float nota = 0;
+        float media;
+
+        Scanner teclado = new Scanner(System.in);
+
+        for (int n =1; n <= 3; n++)
+        {
+            System.out.printf("Digite a nota %d: ", n);
+            nota += teclado.nextFloat();
+        }
+
+        media = nota/3;
+
+        System.out.printf("\nMédia final: %.1f. ", media);
+
+        if (media >= 7)
+        {
+            System.out.println("Aprovado");
+        } else if (media >= 5)
+        {
+            System.out.println("Recuperação");
+        }
+        else
+        {
+            System.out.println("Reprovado");
+        }
+
+    }
+}
