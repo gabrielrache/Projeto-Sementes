48dc1443017afd1b8babee3fb72710ecbd2da69d
Gabriel Carmona <gabrielrache123@hotmail.com>
2025-02-06 23:36:44 -0300

    Conclusão do desafio 1

---
+++ java/desafio_1/README.md
@@ -0,0 +1,7 @@
+![App mercado](https://github.com/user-attachments/assets/ba024e9c-1395-4dfa-a2f7-b1526d545644)
+
+Você irá escrever um programa de console para um mercado que vende apenas três tipos de bebida: cerveja, refrigerante e água.
+O programa deve perguntar qual tipo de bebida a pessoa deseja comprar e caso ela opte pela cerveja o programa deve perguntar a idade da pessoa. Caso a pessoa seja menor de idade deve printar "compra negada". Em todos os outros casos deve printar "compra efetuada com sucesso".
+
+
+
--- java/desafio_1/src/Main.java
+++ java/desafio_1/src/Main.java
@@ -1,6 +1,50 @@
+import java.util.Scanner;
+
 public class Main
 {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main(String[] args)
+    {
+        String produto;
+        int idade;
+        boolean autoriza = false;
+
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.print("""
+                Produtos disponíveis:
+                1 - água
+                2 - refrigerante
+                3 - cerveja
+                
+                Digite a nome ou a opção desejada:\s""");
+
+        produto = teclado.next();
+
+        if (produto.equals("3") | produto.equalsIgnoreCase("cerveja"))
+        {
+            System.out.print("Digite a sua idade: ");
+            idade = teclado.nextInt();
+            autoriza = (idade > 18);
+        }
+        else {
+            if (produto.equals("1")
+                    | produto.equals("2")
+                    | produto.equalsIgnoreCase("refrigerante")
+                    | produto.equalsIgnoreCase("água")
+                    | produto.equalsIgnoreCase("agua")) 
+            {
+                autoriza = true;
+            } 
+            else 
+            {
+                System.out.println("produto não encontrado");
+                System.exit(0);
+            }
+        }
+
+        if (autoriza)
+            System.out.println("Compra efetuada com sucesso");
+        else
+            System.out.println("compra negada");
     }
 }
