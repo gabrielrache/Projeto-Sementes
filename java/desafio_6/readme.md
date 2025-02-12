cc46ce5b21a319243945ad6d743671fa0acc08d4
Gabriel Carmona <gabrielrache123@hotmail.com>
2025-02-12 11:50:30 -0300

    Conclusão do desafio 6

---
+++ java/desafio_6/readme.md
@@ -0,0 +1,6 @@
+Escreva um programa que leia números inteiros fornecidos pelo usuário, de 1 a 10, no console. O programa deve aceitar entradas contínuas e exibir uma mensagem após cada entrada, informando o número digitado. O programa deve parar de solicitar entradas quando o número 10 for digitado.
+
+Regras:
+
+1. O programa só deve aceitar números inteiros de 1 a 10. Caso o usuário insira um número fora desse intervalo ou um valor inválido, exiba uma mensagem de erro e peça novamente a entrada.
+2. Quando o número 10 for digitado, o programa deve exibir uma mensagem de encerramento e terminar sua execução.
---
+++ java/desafio_6/src/Main.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class Main
+{
+    private static int lerNumero(String mensagem)
+    {
+        int leitura;
+        String erro;
+
+        Scanner teclado = new Scanner(System.in);
+
+        do
+        {
+            System.out.print(mensagem);
+
+            try
+            {
+                leitura = teclado.nextInt();
+                break;
+            }
+            catch (Exception e)
+            {
+                erro = teclado.next();
+                System.out.printf("%s não é um número inteiro!\n", erro);
+            }
+        }
+        while (true);
+
+        return leitura;
+    }
+
+    public static void main(String[] args)
+    {
+        int valor;
+
+        do
+        {
+            do
+            {
+                valor = lerNumero("Digite um número inteiro entre 1 a 10: ");
+
+                if (valor <1 | valor > 10)
+                    System.out.printf("%d não está entre 1 e 10\n", valor);
+
+                else break;
+            }
+            while (true);
+
+            if (valor == 10)
+            {
+                System.out.println("Encerramento do programa");
+                break;
+            }
+        }
+        while (true);
+    }
+}
