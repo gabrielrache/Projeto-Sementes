aa691dd96e8beed26a82c0f731b989827f4e7ed8
Gabriel Carmona <gabrielrache123@hotmail.com>
2025-02-13 19:37:03 -0300

    Atualização do método de leitura do desafio 6

--- java/desafio_6/src/Main.java
+++ java/desafio_6/src/Main.java
@@ -1,30 +1,33 @@
 import java.util.Scanner;
 
-public class Main
-{
-    private static int lerNumero()
-    {
+public class Main {
+    private static int lerNumeroEntreAeB(int val_a, int val_b) {
+
         int leitura;
         String erro;
 
         Scanner teclado = new Scanner(System.in);
 
-        do
-        {
-            System.out.print("Digite um número inteiro entre 1 a 10: ");
+        do {
+                do {
+                    System.out.printf("Digite um número inteiro entre %d a %d: ", val_a, val_b);
 
-            try
-            {
-                leitura = teclado.nextInt();
-                break;
-            }
-            catch (Exception e)
-            {
-                erro = teclado.next();
-                System.out.printf("%s não é um número inteiro!\n", erro);
-            }
-        }
-        while (true);
+                    try {
+                        leitura = teclado.nextInt();
+                        break;
+                    }
+                    catch (Exception e) {
+                        erro = teclado.next();
+                        System.out.printf("%s não é um número inteiro!\n", erro);
+                    }
+                }
+                while (true);
+
+                if (leitura < val_a | leitura > val_b)
+                    System.out.printf("%d não está entre %d e %d\n", leitura, val_a, val_b);
+                else
+                    break;
+        } while (true);
 
         return leitura;
     }
@@ -35,16 +38,7 @@ public class Main
 
         do
         {
-            do
-            {
-                valor = lerNumero();
-
-                if (valor <1 | valor > 10)
-                    System.out.printf("%d não está entre 1 e 10\n", valor);
-
-                else break;
-            }
-            while (true);
+            valor = lerNumeroEntreAeB(1,10);
 
             if (valor == 10)
             {
