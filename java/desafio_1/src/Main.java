d2e80c5edec974e18fb4995f8d53a5b2b9d6f3c9
Gabriel Carmona <gabrielrache123@hotmail.com>
2025-02-20 11:07:36 -0300

    Correção do desafio 1

--- java/desafio_1/src/Main.java
+++ java/desafio_1/src/Main.java
@@ -5,7 +5,6 @@ public class Main {
 
         String produto;
         int idade;
-        boolean autoriza = false;
 
         Scanner teclado = new Scanner(System.in);
 
@@ -20,21 +19,18 @@ public class Main {
         produto = teclado.next();
 
         if (produto.equals("3") || produto.equalsIgnoreCase("cerveja")) {
+
             System.out.print("Digite a sua idade: ");
             idade = teclado.nextInt();
-            autoriza = (idade > 18);
-        } else {
-            if (produto.equals("1") || produto.equals("2") || produto.equalsIgnoreCase("refrigerante") || produto.equalsIgnoreCase("água") || produto.equalsIgnoreCase("agua"))
-                autoriza = true;
-            else {
-                System.out.println("produto não encontrado");
-                System.exit(0);
-            }
-        }
 
-        if (autoriza)
+            if (idade >= 18)
+                System.out.println("Compra efetuada com sucesso");
+            else
+                System.out.println("compra negada");
+
+        } else if (produto.equals("1") || produto.equals("2") || produto.equalsIgnoreCase("refrigerante") || produto.equalsIgnoreCase("água") || produto.equalsIgnoreCase("agua"))
             System.out.println("Compra efetuada com sucesso");
         else
-            System.out.println("compra negada");
+            System.out.println("produto não encontrado");
     }
 }
