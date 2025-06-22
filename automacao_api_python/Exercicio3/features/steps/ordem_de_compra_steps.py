6880d75bc155a68969580c27264c6736cc2e6134
Gabriel Carmona <gabriel.carmona@unicred.com.br>
2025-06-21 21:35:07 -0300

    feat (exercicio 3): concluir exercício

--- automacao_api_python/Exercicio3/features/steps/ordem_de_compra_steps.py
+++ automacao_api_python/Exercicio3/features/steps/ordem_de_compra_steps.py
@@ -2,10 +2,10 @@ from behave import *
 from features.impl.ordem_de_compra import OrdemDeCompra
 
 
-@given(u'que o usuário selecionou o animal desejado na petstore')
-def step_impl(context):
+@given(u'que o usuário selecionou o animal com o id correspondente a "{id}" desejado na petstore')
+def step_impl(context, id):
     context.api = OrdemDeCompra()
-    context.api.id = 4
+    context.api.id = id
     context.api.petId = 2
     context.api.quantidade = 10
     context.api.post_criar_uma_nova_ordem()
@@ -14,4 +14,22 @@ def step_impl(context):
 
 @then(u'o sistema valida se a ordem de pedido foi armazenada corretamente')
 def step_impl(context):
+    pass
+
+
+
+@given(u'que o usuário selecionou o id correspondente a "{id}" para remoção da ordem')
+def step_impl(context, id):
+    pass
+
+@given(u'incluiu nova ordem "{id_nova_ordem}" para o animal selecionado na petstore')
+def step_impl(context,id_nova_ordem):
+    pass
+
+@then(u'o sistema valida se a ordem foi removida com sucesso para a "{id}"')
+def step_impl(context, id):
+    pass
+
+@then(u'o sistema valida se a ordem de pedido foi armazenada corretamente com "{id_nova_ordem}"')
+def step_impl(context, id_nova_ordem):
     pass
--- automacao_api_python/Exercicio3/features/usuario.feature
+++ automacao_api_python/Exercicio3/features/usuario.feature
@@ -1,8 +1,28 @@
-# language: pt
+# language: en
 
-Funcionalidade: Efetuar ordem de compra
+Feature: Efetuar ordem de compra
 
-Cenário: Efetuar uma ordem de compra na petstore
-  Dado que o usuário selecionou o animal desejado na petstore
-  Então o sistema valida se a ordem de pedido foi armazenada corretamente
+  Scenario Outline: Efetuar uma ordem de compra na petstore
+    Given que o usuário selecionou o animal com o id correspondente a "<id>" desejado na petstore
+    Then o sistema valida se a ordem de pedido foi armazenada corretamente
 
+    Examples:
+      | id |
+      | 1  |
+      | 2  |
+      | 3  |
+      | 4  |
+
+
+  Scenario Outline:  Efetuar uma remoção de ordem de compra na petstore
+    Given que o usuário selecionou o id correspondente a "<id>" para remoção da ordem
+    And incluiu nova ordem "<id_nova_ordem>" para o animal selecionado na petstore
+    Then o sistema valida se a ordem foi removida com sucesso para a "<id>"
+    Then o sistema valida se a ordem de pedido foi armazenada corretamente com "<id_nova_ordem>"
+
+    Examples:
+      | id | id_nova_ordem |
+      | 1  | 5 |
+      | 2  | 6 |
+      | 3  | 7 |
+      | 4  | 8 |
